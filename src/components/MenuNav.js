3c8b29ef02ce06841cfdccea3f3641b9eb353875
Alberto Beltran <albertobeltran@protonmail.com>
2023-11-07 10:42:52 -0600

    components created

---
+++ src/components/MenuNav.js
@@ -0,0 +1,13 @@
+export default function Menu() {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li><a href="#">Pokedex</a></li>
+                </ul>
+                <input type="text" placeholder="Buscar pokémon">
+                </input>
+            </nav>
+        </>
+    )
+}
---
+++ src/components/Pokemon.js
@@ -0,0 +1,10 @@
+export default function Pokemon() {
+    return (
+        <div className="pokemon">
+            <p className="numero">1</p>
+            <img src="pokemon-img/002.png"/>
+            <p className="nombre">Ivysaur</p>
+            <p className="tipo">Poison, Grass</p>
+        </div>
+    );
+}
